53790ffefbce0a80d2d445c0a96478eb23ed9cf9
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-02-07 12:45:02 +0300

    Add project files.

---
+++ InvalidUsernameException.cs
@@ -0,0 +1,30 @@
+﻿namespace ClassLibrary
+{
+    internal partial class Program
+    {
+        public class InvalidUsernameException : Exception
+        {
+            public string Username { get; set; }
+            public InvalidUsernameException(string? username) : base($"The username '{username}' is invalid ")
+            {
+                Username = username;
+            }
+
+            public InvalidUsernameException(string? username, string? message) : base(message)
+            {
+                Username = username;
+            }
+
+            public InvalidUsernameException(string? username, string? message, Exception? innerException) : base(message, innerException)
+            {
+                Username = username;
+            }
+
+            public InvalidUsernameException()
+            {
+                
+            }
+
+        }
+    }
+}
